bde5923908e6feef978b8b6b6f2e7b05e3de40e4
arunbababa <hatuki.1.gzs@icloud.com>
2025-04-05 11:21:05 +0900

    スコープの課題

--- js/mechanism/udemy-mafia/javascript-essence-tutorial/030_スコープ/910_修了確認_スコープ/start/main.js
+++ js/mechanism/udemy-mafia/javascript-essence-tutorial/030_スコープ/910_修了確認_スコープ/start/main.js
@@ -6,15 +6,29 @@
  * 
  * ※if文は削除してはいけません。
  */
+
+// 自分
 function fn() {
     if(true) {
         let a = 'fn called';
+        return a;
     }
-    return a; // ReferenceError: a is not defined
 }
 
 const result = fn();
 console.log(result);
+// 模範
+
+// function fn() {
+        // let a ;
+//     if(true) {
+//         let a = 'fn called';
+//     }
+        // return a;
+// }
+
+// const result = fn();
+// console.log(result);
 
 /**
  * 問題２：
@@ -26,7 +40,7 @@ function fn2() {
     console.log(val); // 期待値->'val1'
 
     if(true) {
-        var val = 'val2';
+        const val = 'val2'; // varによる変数の巻き上げを解消
         console.log(val); // 期待値->'val2'
     }
 
@@ -45,4 +59,33 @@ fn2();
  * increment(); // 期待値->3
  * increment(); // 期待値->4
  */
+
+// 自分　もぉ前のレクチャーの完コピペ完コピペやんアウト
+// function incrementFactory() {
+//     let num = 0;
+//     function a() {
+//         num = num + 1;
+//         console.log(num);
+//     }
+//     return a;
+// }
+// const increment = incrementFactory();
+// increment();
+// increment();
+// increment();
+// increment();
+
+// 模範
+{
+    num = 0;
+    function increment(){
+        num += 1;
+        console.log(num)
+    }
+}
+
+increment()
+increment()
+increment()
+increment()
 
