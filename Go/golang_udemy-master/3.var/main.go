bd18ba9f5f6479e9e1e77e42aa0cc6a2266cbae1
arunbababa <hatuki.1.gzs@icloud.com>
2025-04-08 09:47:59 +0900

    Go基礎:変数

--- Go/golang_udemy-master/3.var/main.go
+++ Go/golang_udemy-master/3.var/main.go
@@ -7,6 +7,7 @@ var s string = "Golang"
 
 var t, f bool = true, false
 
+// ()でインポートと同様に複数同時にかける、なおこれを出力する際は改行されないのだ（fmt.println）
 var (
 	ii int    = 1000
 	ss string = "Go"
@@ -14,7 +15,7 @@ var (
 
 var i2 int
 
-var sss string
+var sss string = "arunbababa"
 
 //i3 := 100
 
@@ -33,8 +34,8 @@ func main() {
 	i3 := 200
 	fmt.Println(i3)
 
-	//i3 := 300
-
+	//i3 := 300 ← 暗黙的な定義 これで値に応じて型が当たる、ただ同的に当たるわけではないので注意 なお同じ型での値の更新はできる、ただ再宣言と違う型の代入は無理
+	// あと関数内でしか当たらないとのこと　関数外でやる際は明示的な定義をしろ　あと基本的には明示的に型付けしろ
 	i3 = 300
 
 	//i3 = "string"
