0d5aeb8c5e040fb86e32d1c5d446d96deaa1f154
arunbababa <hatuki.1.gzs@icloud.com>
2025-04-08 12:49:22 +0900

    すべてコミット：基礎系/この後TSに切り替える

--- Go/golang_udemy-master/4.basetype/1.main.go
+++ Go/golang_udemy-master/4.basetype/1.main.go
@@ -19,4 +19,9 @@ func main() {
 	fmt.Println(`"`)
 
 	//fmt.Println(s[0])
+
+	// int型にもいろいろ種類があり、その中で型があっていないといけない、なお型変換できる、そして型調べだがfmt.Printlnの第二引数に変数名渡せば型が調べられる
+	var mine int64 = 100
+	var mien_vani = 100
+	fmt.Println(mine + int64(mien_vani))
 }
