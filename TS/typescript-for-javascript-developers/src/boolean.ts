ed118b2fd5dbdb3c0c50161e2e1f318e1652c02f
arunbababa <hatuki.1.gzs@icloud.com>
2025-04-10 03:12:28 +0900

    boolean

--- TS/typescript-for-javascript-developers/src/boolean.ts
+++ TS/typescript-for-javascript-developers/src/boolean.ts
@@ -1,8 +1,15 @@
-export {};
+export { };
+// なぜexportが必要なのか？
+// JSではデフォルトでファイルがモジュールとして認識されない
+// のでモジュールシステムを利用しimportやらexportでモジュール化しないといけない
+// だから上のexport{}をなくすとnameの行でエラーが出るなぜか？
+// モジュールでないつまりモジュールスコープでないさらに上のnameを変更というかまぁアクセスなり定義ナイスする可能性がある
+// そのさらに上の奴はカーソル充てればわかるよ
+// これはあかんよね危ない、ということでエラーが出る
 
 let name = 'TypeScript';
 
-let isFinished: boolean = true;
-isFinished = false;
-// isFinished = 1;
-console.log({ isFinished });
+let isFinished: boolean = false;
+isFinished = true;
+// isFinished = 1; // truthyはちゃうぞぉ
+console.log({ isFinished }); // なおこれはオブジェクト生成の省略記法
