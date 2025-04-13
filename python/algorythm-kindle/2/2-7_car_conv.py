159b51aeae284d85540467a73d399bc1d59987a3
arunbababa <hatuki.1.gzs@icloud.com>
2025-04-13 23:12:21 +0900

    add: 進数変換関数を利用してユーザと対話的にするプログラムを実装

--- python/algorythm-kindle/2/2-7_car_conv.py
+++ python/algorythm-kindle/2/2-7_car_conv.py
@@ -16,3 +16,25 @@ print(card_conv(8921, 2))
 # 自分なりの理解を追加
 # 数学をブログラム言語に変換したのみ
 # 気づけなかったのはｂ最後の部分を反転させて出力しないと行けないところ
+
+# 2-7[B]ユーザと対話的に基数変換を行う
+if __name__ == '__main__':
+    
+    print('10進数を基数変換します。')
+    
+    while True:
+        while True:
+            no = int(input('変換する非負の整数'))
+            if no > 0:
+                break
+            
+        while True:
+            cd = int(input('何進数に変換しますか？(2~36)'))
+            if 2 <= cd <= 36:
+                break
+            
+        print(f'{cd}進数では{card_conv(no, cd)}です。')
+        
+        retry = input('もう一度処理を実行しますか？(Y / N)')
+        if retry in {'N', 'n'}:
+            break
