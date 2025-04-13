1969e4b016fd7549a05d09bf18d0094732b5f523
arunbababa <hatuki.1.gzs@icloud.com>
2025-04-13 21:57:26 +0900

    add: 進数変換関数の作成/模範

---
+++ python/algorythm-kindle/2/2-7_car_conv.py
@@ -0,0 +1,14 @@
+# 読み込んだ10進数を2進数~32進数へと基数変換して表示する
+
+def card_conv(x:int, r:int) -> str:
+    """整数値xをr進数に変換した数値を表す文字列を返却"""
+    d = ''
+    dchar = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    
+    while x > 0:
+        d += dchar[x % r]
+        x //= r
+        
+    return d[::-1]
+        
+print(card_conv(8921, 2))
