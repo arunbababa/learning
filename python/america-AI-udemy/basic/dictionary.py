7ce5f0cec2a916ca48192b1b49a8e8e647bc4a1a
arunbababa <hatuki.1.gzs@icloud.com>
2025-04-14 06:54:14 +0900

    add:python/amerika-AI-udemy:dictionaryメソッドの練習問題を解く

--- python/america-AI-udemy/basic/dictionary.py
+++ python/america-AI-udemy/basic/dictionary.py
@@ -10,3 +10,38 @@
 # update()メソッド → 辞書の結合ができる
 辞書1.update(辞書2)で使う
 """
+
+# チャレンジ問題 whileのプログラムを辞書を使ってより良いものにしていく
+age = int(input("何歳ですか？:"))
+casino_age = 18
+games = {
+    1:'ルーレット',
+    2:'ブラックジャック',
+    3:'ポーカー',
+    }
+game_text = """プレイするゲームを選択してください. 
+1: ルーレット
+2: ブラックジャック 
+3: ポーカー
+:"""
+
+if age >= casino_age:
+    print("どうぞお入りください")
+    while True:
+        game = input(game_text)
+        if game not in {'1','2','3'}:
+            print('1,2,3の中からお選びください')
+            continue
+        if game == '1':
+            print(f"あなたは{games[int(game)]}を選びました")
+            break
+        elif game == '2':
+            print(f"あなたは{games[int(game)]}を選びました")
+            break
+        elif game == '3':
+            print(f"あなたは{games[int(game)]}を選びました")
+            break
+        else:
+            print("1~3を選んでください")
+else:
+    print(f"{casino_age}歳未満の方はカジノへは入れません！")
