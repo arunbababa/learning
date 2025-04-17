6527ec139e1ac84a432ab054caa12d37dc0dfab6
arunbababa <hatuki.1.gzs@icloud.com>
2025-04-17 21:00:11 +0900

    add: type alias

--- TS/typescript-for-javascript-developers/src/type-aliases.ts
+++ TS/typescript-for-javascript-developers/src/type-aliases.ts
@@ -1,9 +1,9 @@
-export {};
+export { };
 
 type Mojiretsu = string;
 
 const fooString: string = 'Hello';
-const fooMojiretsu: Mojiretsu = 'Hello';
+const fooMojiretsu: Mojiretsu = 'Hello'; // string型をMojiretu型として設定できる実態はただのstring型
 
 const example1 = {
   name: 'Ham',
@@ -18,6 +18,7 @@ type Profile = {
 const example2: Profile = {
   name: 'Ham',
   age: 43
+  //height: 175 // これはNG、Profile型にheightプロパティはないため
 };
 
-type Profile2 = typeof example1;
+type Profile2 = typeof example1; // example1の型をProfile2として設定できる、つまりexample1の型をProfile2として設定できる、これで依存関係は生まれるが追従するようにできる
