2fc5bab7c71a7b3e1c9907d24a3387dbe302edfc
arunbababa <hatuki.1.gzs@icloud.com>
2025-04-17 20:41:53 +0900

    add: type of object

--- TS/typescript-for-javascript-developers/src/object.ts
+++ TS/typescript-for-javascript-developers/src/object.ts
@@ -1,9 +1,11 @@
-export {};
+export { };
 
 let profile1: object = { name: 'Ham' };
 profile1 = { birthYear: 1976 };
+console.log(profile1); // オブジェクト型である設定はできるが更新は可能、当然
 
 let profile2: {
   name: string;
 } = { name: 'Ham' };
-profile2 = { name: 'Nimo' };
+profile2 = { name: 'Nimo' }; // これはOK
+// profile2 = { birthYear: 1976 }; // これはNG、nameプロパティがないため、つまりこっちはオブジェクトでありかつプロパティの指定までできる
