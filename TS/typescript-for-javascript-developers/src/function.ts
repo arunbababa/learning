0ae80461d5018ac82697efbf57eee282987a45b1
arunbababa <hatuki.1.gzs@icloud.com>
2025-04-20 23:48:50 +0900

    add: type in function

--- TS/typescript-for-javascript-developers/src/function.ts
+++ TS/typescript-for-javascript-developers/src/function.ts
@@ -1,7 +1,15 @@
-export {};
+export { };
 
 function bmi(height: number, weight: number): number {
   return weight / (height * height);
 }
 
 console.log(bmi(1.78, 86));
+
+// ハムさんいわく、関数の型について注意すべきは引数の型と戻り値の型のみ
+// 個人的なメモ　→　んーなんか関数自体の型定義ってあったよな？どんな引数とってどんな戻り値を返すかみたいなやつ
+
+// これだ
+// const bmi: (height: number, weight: number) => number = (height, weight) => {
+//   return weight / (height * height);
+// };
