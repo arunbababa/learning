5b7550a0c412af8d0398b52d01ae8fdd12ef651a
arunbababa <hatuki.1.gzs@icloud.com>
2025-04-24 00:00:25 +0900

    add: default-param

--- TS/typescript-for-javascript-developers/src/default-parameters.ts
+++ TS/typescript-for-javascript-developers/src/default-parameters.ts
@@ -1,7 +1,9 @@
-export {};
+export { };
 
 const nextYearSalary = (currentSalary: number, rate: number = 1.1) => {
   return currentSalary * rate;
 };
 
 console.log(nextYearSalary(1000));
+
+// こんな感じで第二引数を渡さない場合は?となる、どちらでもよいとのこと
