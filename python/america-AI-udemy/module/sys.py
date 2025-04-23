ab0a939ac9f272e5f6288c3808c37da4af4ca545
arunbababa <hatuki.1.gzs@icloud.com>
2025-04-23 19:04:00 +0900

    add: how to function the sys module

---
+++ python/america-AI-udemy/module/sys.py
@@ -0,0 +1,7 @@
+import sys
+
+print(sys.path)
+# これで出力確認すると次へのパスが確認できる
+# C:\\Users\\hatuk\\AppData\\Local\\Programs\\Python\\Python312\\Lib'
+# とりまsysにappendで自分のモジュールを適当に追加すれば参照できるようになるよ
+# c:\\Users\\hatuk\\Dev\\mine\\what-i-learned\\python\\~~←自分で指定したパス
