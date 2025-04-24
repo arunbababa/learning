46e793e794686803cefa2d8f190e487e329961a8
arunbababa <hatuki.1.gzs@icloud.com>
2025-04-24 12:03:04 +0900

    add: how to define class in TS

--- TS/typescript-for-javascript-developers/src/my-first-class.ts
+++ TS/typescript-for-javascript-developers/src/my-first-class.ts
@@ -1,12 +1,16 @@
-export {};
+export { };
 
 class Person {
-  name: string;
-  age: number;
+  // name: string;
+  // age: number;
 
-  constructor(name: string, age: number) {
-    this.name = name;
-    this.age = age;
+  // constructor(name: string, age: number) { // なんか説明に合ったけどconstructor関数の戻り値の型定義はしないとのこと、まぁ特に何もreturnしないしね
+  //   this.name = name;
+  //   this.age = age;
+  // }
+
+  constructor(public name: string, public age: number) {
+    // この書き方でプロパティ宣言と代入をまとめてできる
   }
 
   profile(): string {
