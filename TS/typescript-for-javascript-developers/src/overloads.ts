83180e1a92577a8cc6ade0fad99d59d625027e15
arunbababa <hatuki.1.gzs@icloud.com>
2025-04-24 11:14:22 +0900

    add: how to use overload/シグネチャの型定義による引数の守り方

--- TS/typescript-for-javascript-developers/src/overloads.ts
+++ TS/typescript-for-javascript-developers/src/overloads.ts
@@ -1,17 +1,20 @@
-export {};
+export { };
 
 function double(value: number): number;
 function double(value: string): string;
 
+// 実態関数の方でanyとしたら型安全じゃなくない？
+// 違う!!上の2つですでにシグネチャとして実態の方で何を受け取れるか型指定済み
+// なので配列とか渡すと普通にエラーしてくれるのだ!!
 function double(value: any): any {
   console.log(typeof value);
   if (typeof value === 'number') {
     return value * 2;
   } else {
-    return value + value;
+    return value + value; // あ、文字列は * 使えないんやな
   }
 }
 
 console.log(double(100));
 console.log(double('Go '));
-// console.log(double(true));
+// console.log(double(true));　これそもそも渡せない、シグネチャが守ってくれる
