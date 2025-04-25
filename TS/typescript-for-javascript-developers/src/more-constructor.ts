aaeb2eed83b1d56e64afe5e4075031df9c16bd33
arunbababa <hatuki.1.gzs@icloud.com>
2025-04-26 05:44:11 +0900

    add: know deeply about contructor

--- TS/typescript-for-javascript-developers/src/more-constructor.ts
+++ TS/typescript-for-javascript-developers/src/more-constructor.ts
@@ -1,7 +1,12 @@
-export {};
+export { };
 
 class Person {
-  constructor(public name: string, protected age: number) {}
+  constructor(public name: string, protected age: number) { }
+  // 以下のように書かずに済むんだぜ
+  // constructor(name:string, age:number){
+  //   this.name = name;
+  //   this.age = age;
+  // }
 }
 
 const me = new Person('はむさん', 43);
