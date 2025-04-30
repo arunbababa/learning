0f57fa555beec3a514389b1d0ed09b1d9ee1e51b
arunbababa <hatuki.1.gzs@icloud.com>
2025-04-30 17:36:17 +0900

    add: what is static method in TS

--- TS/typescript-for-javascript-developers/src/static-members.ts
+++ TS/typescript-for-javascript-developers/src/static-members.ts
@@ -1,4 +1,4 @@
-export {};
+export { };
 
 class Me {
   static isProgrammer: boolean = true;
@@ -6,12 +6,14 @@ class Me {
   static lastName: string = 'Ishida';
 
   static work() {
-    //     "Hey, guys! This is Atsushi! Are you interested in TypeScript? Let's dive into TypeScript!";
-    return `Hey, guys! This is ${this.firstName}! Are you interested in TypeScript? Let's dive into TypeScript!`;
+    // "Hey, guys! This is Atsushi! Are you interested in TypeScript? Let's dive into TypeScript!";
+    console.log(`Hey, guys! This is ${this.firstName}! Are you interested in TypeScript? Let's dive into TypeScript!`);
+    console.log(`Hey, guys! This is ${Me.firstName}! Are you interested in TypeScript? Let's dive into TypeScript!`);
+    return;
   }
 }
 
 // let me = new Me();
-// console.log(me.isProgrammer);
+// console.log(me.isProgrammer); これだとなぜダメなのか？　isProgramerはインスタンスからアクセスしては行けないからである！静的メンバだもん
 console.log(Me.isProgrammer);
 console.log(Me.work());
