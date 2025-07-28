e3a9e51a09eb4e3c1987212a5ad8ddbebd43b512
arunbababa <hatuki.1.gzs@icloud.com>
2025-07-28 22:03:08 +0900

    first commit for スッキリjava: 日本語なのでコミットメッセージが変化も

---
+++ スッキリわかるJava入門第4章/Aru.java
@@ -0,0 +1,9 @@
+package learning.スッキリわかるJava入門第4章;
+
+public class Aru {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        // このSystemはどこからきているか?
+    }
+}
---
+++ スッキリわかるJava入門第4章/Main.java
@@ -0,0 +1,11 @@
+// この下の分追加したら治ったなぜだ?要調査
+package learning.スッキリわかるJava入門第4章;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        // このSystemはどこからきているか?
+
+    }
+}
