1ba2e3c3033009550244636d4a8684bec47401c9
arunbababa <hatuki.1.gzs@icloud.com>
2025-07-31 18:33:08 +0900

    add files with some memo

--- スッキリわかるJava入門第4章/Aru.java
+++ スッキリわかるJava入門第4章/Aru.java
@@ -1,9 +1,13 @@
-package learning.スッキリわかるJava入門第4章;
-
+// パッケージインストールはなくてOKむしろあると実行ディレクトリげ限定というかまぁパスしてだるいのでなしでOK
+// このファイルを実行したかったら
+// javac Main
+// java Main 'arugs用の適当な'
+// ってかんじ
 public class Aru {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
         // このSystemはどこからきているか?
+
     }
 }
--- スッキリわかるJava入門第4章/Main.java
+++ スッキリわかるJava入門第4章/Main.java
@@ -1,9 +1,8 @@
 // この下の分追加したら治ったなぜだ?要調査
-package learning.スッキリわかるJava入門第4章;
-
 public class Main {
+    // (String[] argsはなんなのか
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(args[0]);
 
         // このSystemはどこからきているか?
 
